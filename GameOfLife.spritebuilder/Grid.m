684912ae46e4fff2af67b55691670e3fc6b889c0
jessicajiang <jessicajiang@Xuepings-MacBook-Pro.local>
2014-06-25 12:43:21 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -70,127 +70,125 @@ static const int GRID_COLUMNS = 10;
         y += _cellHeight;
     }
 }
-    
+
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
-    {
-        //get the x,y coordinates of the touch
-        CGPoint touchLocation = [touch locationInNode:self];
-        
-        //get the Creature at that location
-        Creature *creature = [self creatureForTouchPosition:touchLocation];
-        
-        //invert it's state - kill it if it's alive, bring it to life if it's dead.
-        creature.isAlive = !creature.isAlive;
-    }
+{
+    //get the x,y coordinates of the touch
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    //get the Creature at that location
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    
+    //invert it's state - kill it if it's alive, bring it to life if it's dead.
+    creature.isAlive = !creature.isAlive;
+}
 
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
-    {
-        //get the row and column that was touched, return the Creature inside the corresponding cell
-        //Divide the y coordinate of the touch (accessed as touchPosition.y) by the cellHeight to get the row that was touched. Store that value in an integer called row. Divide the x coordinate of the touch (accessed as touchPosition.x) by the cellWidth to get the column that was touched. Store that value in an integer called column.You can now return the creature with the statement return _gridArray[row][column].
-        int row = touchPosition.y / _cellHeight;
-        int column = touchPosition.x / _cellWidth;
-        return _gridArray[row][column];
-    }
-    
--(void)evolveStep
-  {
-      //update each Creature's neighbor count
-      [self countNeighbors];
-      
-      //update each Creature's state
-      [self updateCreatures];
-      
-      //update the generation so the label's text will display the correct generation
-      _generation++;
-  }
+{
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    //Divide the y coordinate of the touch (accessed as touchPosition.y) by the cellHeight to get the row that was touched. Store that value in an integer called row. Divide the x coordinate of the touch (accessed as touchPosition.x) by the cellWidth to get the column that was touched. Store that value in an integer called column.You can now return the creature with the statement return _gridArray[row][column].
+    int row = touchPosition.y / _cellHeight;
+    int column = touchPosition.x / _cellWidth;
+    return _gridArray[row][column];
+}
 
+-(void)evolveStep
+{
+    //update each Creature's neighbor count
+    [self countNeighbors];
     
+    //update each Creature's state
+    [self updateCreatures];
+    
+    //update the generation so the label's text will display the correct generation
+    _generation++;
+}
+
+
 - (BOOL)isIndexValidForX:(int)x andY:(int)y
+{
+    BOOL isIndexValid = YES;
+    if(x < 0 || y < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS)
     {
-        BOOL isIndexValid = YES;
-        if(x < 0 || y < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS)
-        {
-            isIndexValid = NO;
-        }
-        return isIndexValid;
+        isIndexValid = NO;
     }
-    
-    
+    return isIndexValid;
+}
+
+
 -(void)countNeighbors
+{
+    // iterate through the rows
+    // note that NSArray has a method 'count' that will return the number of elements in the array
+    for (int i = 0; i < [_gridArray count]; i++)
     {
-        // iterate through the rows
-        // note that NSArray has a method 'count' that will return the number of elements in the array
-        for (int i = 0; i < [_gridArray count]; i++)
+        // iterate through all the columns for a given row
+        for (int j = 0; j < [_gridArray[i] count]; j++)
         {
-            // iterate through all the columns for a given row
-            for (int j = 0; j < [_gridArray[i] count]; j++)
+            // access the creature in the cell that corresponds to the current row/column
+            Creature *currentCreature = _gridArray[i][j];
+            
+            // remember that every creature has a 'livingNeighbors' property that we created earlier
+            currentCreature.livingNeighbors = 0;
+            
+            // now examine every cell around the current one
+            
+            // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
+            for (int x = (i-1); x <= (i+1); x++)
             {
-                // access the creature in the cell that corresponds to the current row/column
-                Creature *currentCreature = _gridArray[i][j];
-                
-                // remember that every creature has a 'livingNeighbors' property that we created earlier
-                currentCreature.livingNeighbors = 0;
-                
-                // now examine every cell around the current one
-                
-                // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
-                for (int x = (i-1); x <= (i+1); x++)
+                // go through the column to the left of the current cell, the column the cell is in, and the column to the right of the current cell
+                for (int y = (j-1); y <= (j+1); y++)
                 {
-                    // go through the column to the left of the current cell, the column the cell is in, and the column to the right of the current cell
-                    for (int y = (j-1); y <= (j+1); y++)
+                    // check that the cell we're checking isn't off the screen
+                    BOOL isIndexValid;
+                    isIndexValid = [self isIndexValidForX:x andY:y];
+                    
+                    // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
+                    if (!((x == i) && (y == j)) && isIndexValid)
                     {
-                        // check that the cell we're checking isn't off the screen
-                        BOOL isIndexValid;
-                        isIndexValid = [self isIndexValidForX:x andY:y];
-                        
-                        // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
-                        if (!((x == i) && (y == j)) && isIndexValid) 
+                        Creature *neighbor = _gridArray[x][y];
+                        if (neighbor.isAlive)
                         {
-                            Creature *neighbor = _gridArray[x][y];
-                            if (neighbor.isAlive)
-                            {
-                                currentCreature.livingNeighbors += 1;
-                            }
+                            currentCreature.livingNeighbors += 1;
                         }
                     }
                 }
             }
         }
     }
-    
+}
+
 -(void)updateCreatures
+{
+    int numAlive = 0;
+    //You will need to create a double-nested for-loop like we did in countNeighbors to access every creature in the Grid. Look over the code in countNeighbors if you need a refresher on how to do that.
+    // iterate through the rows
+    // note that NSArray has a method 'count' that will return the number of elements in the array
+    for (int i = 0; i < [_gridArray count]; i++)
     {
-        int numAlive = 0;
-        //You will need to create a double-nested for-loop like we did in countNeighbors to access every creature in the Grid. Look over the code in countNeighbors if you need a refresher on how to do that.
-        // iterate through the rows
-        // note that NSArray has a method 'count' that will return the number of elements in the array
-        for (int i = 0; i < [_gridArray count]; i++)
+        // iterate through all the columns for a given row
+        for (int j = 0; j < [_gridArray[i] count]; j++)
         {
-            // iterate through all the columns for a given row
-            for (int j = 0; j < [_gridArray[i] count]; j++)
+            // access the creature in the cell that corresponds to the current row/column
+            Creature *currentCreature = _gridArray[i][j];
+            
+            //In the if statement, check if the Creature's livingNeighbors property is set to 3. If it is, that means it has 3 live neighbors so you want to set its isAlive property to TRUE. In the else if you want to check if the Creature has less than or equal to 1 living neighbors or more than or equal to 4. If either are true, set the Creature's isAlive property to FALSE.
+            if (currentCreature.livingNeighbors == 3 || currentCreature.livingNeighbors== 2)
             {
-                // access the creature in the cell that corresponds to the current row/column
-                Creature *currentCreature = _gridArray[i][j];
+                currentCreature.isAlive = @true;
+                numAlive++;
+            }
+            else
+            {
+                currentCreature.isAlive = @false;
                 
-                //In the if statement, check if the Creature's livingNeighbors property is set to 3. If it is, that means it has 3 live neighbors so you want to set its isAlive property to TRUE. In the else if you want to check if the Creature has less than or equal to 1 living neighbors or more than or equal to 4. If either are true, set the Creature's isAlive property to FALSE.
-                if (currentCreature.livingNeighbors == 3)
-                {
-                    currentCreature.isAlive = @true;
-                    numAlive++;
-                }
-                else
-                {
-                    if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4)
-                    {
-                        currentCreature.isAlive = @false;
-                        numAlive++;
-                    }
-                }
             }
             
         }
-        _totalAlive = numAlive;
+        
     }
+    _totalAlive = numAlive;
+}
 
 
 @end
